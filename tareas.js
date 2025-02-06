26cc7216271589cea47dd30186b5beb76dca49d5
Abisai <Al03095697@tecmilenio.mx>
2025-02-06 08:18:05 -0600

    agregue tareas

---
+++ tareas.js
@@ -0,0 +1,26 @@
+[
+    {
+        "id": "1",
+        "title": "Comprar leche",
+        "description": "Ir al supermercado y comprar leche",
+        "completed": false
+    },
+    {
+        "id": "2",
+        "title": "Estudiar para el examen",
+        "description": "Dedicar al menos 2 horas a estudiar para el examen de matemáticas",
+        "completed": false
+    },
+    {
+        "id": "3",
+        "title": "Hacer ejercicio",
+        "description": "Ir al gimnasio y hacer una rutina de 1 hora",
+        "completed": true
+    },
+    {
+        "id": "4",
+        "title": "Llamar a mamá",
+        "description": "Llamar a mi mamá para saber cómo está",
+        "completed": false
+    }
+]
